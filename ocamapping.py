f3fd212057badb682c238c9ba1374fbd0f604e21
samuelsh <samuel.sh79@gmail.com>
2016-03-15 09:31:36 +0200

    Initial Commit

---
+++ ocamapping.py
@@ -0,0 +1,213 @@
+#!/usr/bin/python
+import os
+import subprocess
+import os.path
+import sys
+import threading
+import Queue
+from optparse import OptionParser
+import hanging_threads
+
+MAX_THREADS = 16
+
+
+def fscat(options, queue, results_q, name, is_multithread=True):
+    problematic_ranges = []
+    while not queue.empty():
+        try:
+            file = queue.get()
+            print name + ": getting %s from queue" % file
+            if not os.path.isfile(file):
+                continue
+            rangeslength = [0, 0, 0]  # Null, oca, disk
+            output = "file " + file
+            problem = False
+            over64KBwithoutOCAorNULL = False
+            command = "fscat"
+            p = subprocess.Popen([command, "-M", file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            c = p.communicate()
+            if p.returncode != 0:
+                if options.verbose:
+                    print c[1]
+                    print c[0]
+                print name + ": fscat returned " + str(p.returncode) + " for file " + file + ", skipping."
+                continue
+            if options.fscatoutput:
+                print c[1]
+                print c[0]
+            if len(c[0]) == 0:
+                output += " is inline"
+            startofdiskrange = 0
+            for line in c[0].splitlines():
+                line = line.split(" -> ")
+                line = [line[0].split("{Mapping: [")[1].split("]")[0].split("-"), line[1]]
+                if not line[0][1] == "8000000000000000":  # last range points outside the file and is always Null
+                    #  - just ignore it
+                    line[0] = [int(x, 16) for x in line[0]]
+                    rangelen = line[0][1] - line[0][0]
+                    if "-sec" in line[1]:
+                        print name + ": " + output + " contains redundant data!"
+                        continue
+                    if "oca" in line[1]:
+                        rangeslength[1] = rangeslength[1] + rangelen
+                        startofdiskrange = line[0][1]  # reset consecutive disk range count
+                    elif "[0-0]" in line[1]:
+                        rangeslength[0] = rangeslength[0] + rangelen
+                        startofdiskrange = line[0][1]
+                    elif "disk" in line[1]:
+                        rangeslength[2] = rangeslength[2] + rangelen
+                        if (line[0][
+                                1] - startofdiskrange) >= 65536:  # consecutive disk range larger than 64KB - shouldn't happen
+                            # after dedup for files of at least 64KB
+                            problem = True
+                            problematic_ranges.append("[ %x - %x ]" % (int(line[0][0]), int(line[0][1])))
+                    elif "shared null" in line[1] or "owned null" in line[1]:  # Skipping "shared null" lines
+                        startofdiskrange = line[0][1]
+                        if options.verbose:
+                            print name + ": " + output + " null mapping"
+                    else:
+                        print name + ": " + output + " parsing error!"
+                        print "Parsing error DEBUG: %s \n" % line[1]
+                        break
+            output = "Null ranges: " + str(rangeslength[0]) + " \tOca ranges: " + str(
+                rangeslength[1]) + " \tDisk ranges: " + str(rangeslength[2]) + " " + output
+            if problem or options.filesize or (
+                                options.outputnondeduped and rangeslength[0] == 0 and rangeslength[1] == 0 and
+                            rangeslength[
+                                2] >= 65536):
+                filesize = os.path.getsize(file)
+                output = output + " Size: " + str(filesize)
+                if filesize < 65536:  # file too small to be deduped (e.g. a 63KB file is mapped to a 64KB disk range)
+                    problem = False
+                else:
+                    if rangeslength[0] == 0 and rangeslength[1] == 0 and rangeslength[2] >= 65536:
+                        over64KBwithoutOCAorNULL = True
+                        output += " is over 64KB without OCA or NULL ranges!!!"
+                if problem:
+                    if options.verbose:
+                        for mrange in problematic_ranges:  # Will print all ranges that are expected to be deduped
+                            output += " contains a disk range %s that should have been deduped!\n" % mrange
+                    else:
+                        output += " contains a disk range/s that should have been deduped!"
+            if options.verbose or problem or over64KBwithoutOCAorNULL:
+                print name + ": " + output
+
+            if is_multithread is False:  # in case function is called as single execution, we won't loop
+                break
+
+        except Exception as e:
+            raise e
+
+    results_q.put(problem)
+    print name + ": finished"
+
+
+def run_recursive_scan(options, queue, results_q):
+    thread_pool = []
+
+    for dirpath, dirnames, filenames in os.walk(options.path):
+        for name in filenames:
+            queue.put(os.path.join(dirpath, name))
+
+    for i in range(0, MAX_THREADS):
+        t = threading.Thread(target=fscat, name=("thread-%d" % i), args=(options, queue, results_q, ("thread-%d" % i)))
+        thread_pool.append(t)
+
+    for t in thread_pool:
+        print "thread %s started" % t.getName()
+        t.start()
+
+    for t in thread_pool:
+        t.join()
+
+    while not results_q.empty():
+        q = results_q.get()
+        if q is True:  # if 'True', there is a problem
+            return q
+
+
+def run_single_folder_scan(options, queue, results_q):
+    thread_pool = []
+
+    for name in os.listdir(options.path):
+        queue.put(os.path.join(options.path, name))
+
+    for i in range(0, MAX_THREADS):
+        t = threading.Thread(target=fscat, name=("thread-%d" % i), args=(options, queue, results_q, ("thread-%d" % i)))
+        thread_pool.append(t)
+
+    for t in thread_pool:
+        print "thread %s started" % t.getName()
+        t.start()
+
+    for t in thread_pool:
+        t.join()
+
+    while not results_q.empty():
+        q = results_q.get()
+        if q is True:
+            return q
+
+
+def run_single_file_scan(options, queue, results_q):
+    queue.put(options.path)
+
+    t = threading.Thread(target=fscat, name="thread-1", args=(options, queue, results_q, "thread-1", False))
+
+    print "thread %s started" % t.getName()
+    t.start()
+    t.join()
+
+    q = results_q.get()
+    if q is True:
+        return q
+
+
+def main():
+    """ Main function
+    Returns:
+    0 - all relevant files are deduped
+    1 - dedup error
+    2 - bad argument passed
+    """
+
+    queue = Queue.Queue()
+    results_q = Queue.Queue()
+
+    parser = OptionParser()
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose")
+    parser.add_option("-r", "--recursive", action="store_true", dest="recursive")
+    parser.add_option("-f", "--fscatoutput", action="store_true", dest="fscatoutput", help="Print fscat -M's output")
+    parser.add_option("-s", "--filesize", action="store_true", dest="filesize", help="Always print file size")
+    parser.add_option("-p", "--path", help="file/dir path", action="store", dest="path", default=".")
+    parser.add_option("-o", "--outputnondeduped", action="store_true", dest="outputnondeduped",
+                      help="Indicates files that are over 64KB, without OCA or NULL ranges")
+    (options, args) = parser.parse_args()
+
+    if options.path[0] != "/":
+        options.path = os.path.abspath(options.path)
+        print "Using path: " + options.path
+    if options.path[:9] != "/mnt/mgmt":
+        print "Path should start with /mnt/mgmt"
+        sys.exit(2)
+
+    if os.path.isfile(options.path):
+        print "scanning single file..."
+        if run_single_file_scan(options, queue, results_q) is True:
+            sys.exit(1)
+    elif os.path.isdir(options.path):
+        if options.recursive:
+            print "Scanning directory tree..."
+            if run_recursive_scan(options, queue, results_q) is True:
+                sys.exit(1)
+        else:
+            print "Scanning folder for files..."
+            if run_single_folder_scan(options, queue, results_q) is True:
+                sys.exit(1)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print e
