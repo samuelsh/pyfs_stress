4e875a971577147e12d19a228a476abd4d521fc5
samuelsh <samuel.sh79@gmail.com>
2016-03-21 15:10:57 +0200

    multiprocessing folder scan

--- ocamapping.py
+++ ocamapping.py
@@ -137,7 +137,7 @@ def run_recursive_scan(options, queue, results_q):
         unsearched.put(path)
 
     for i in range(MAX_PROCESSES):
-        folders_scan_pool.apply_async(dir_scan_worker, args=(i,))
+        folders_scan_pool.apply_async(dir_scan_worker, args=[i])
 
     # for dirpath, dirnames, filenames in os.walk(options.path):
     #     for name in filenames:
