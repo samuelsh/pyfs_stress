a448351ec7358251f7382ea1ec5381260c6753f9
samuelsh <samuel.sh79@gmail.com>
2016-03-22 11:51:57 +0200

    multiprocessing folder scan

---
+++ dir_walker.py
@@ -0,0 +1,48 @@
+from multiprocessing.pool import Pool
+from multiprocessing import JoinableQueue as Queue
+import os
+from optparse import OptionParser
+
+
+def explore_path(path):
+    directories = []
+    nondirectories = []
+    for filename in os.listdir(path):
+        fullname = os.path.join(path, filename)
+        if os.path.isdir(fullname):
+            directories.append(fullname)
+        else:
+            nondirectories.append(filename)
+    outputfile = path.replace(os.sep, '_') + '.txt'
+    with open(outputfile, 'w') as f:
+        for filename in nondirectories:
+            print >> f, filename
+    return directories
+
+
+def parallel_worker():
+    while True:
+        path = unsearched.get()
+        dirs = explore_path(path)
+        for newdir in dirs:
+            unsearched.put(newdir)
+        unsearched.task_done()
+
+
+parser = OptionParser()
+parser.add_option("-p", "--path", help="file/dir path", action="store", dest="path", default=".", requred=True)
+(options, args) = parser.parse_args()
+
+# acquire the list of paths
+first_level_dirs = next(os.walk(options.path))[1]
+
+unsearched = Queue()
+for path in first_level_dirs:
+    unsearched.put(path)
+
+pool = Pool(5)
+for i in range(5):
+    pool.apply_async(parallel_worker)
+
+unsearched.join()
+print 'Done'
--- ocamapping.py
+++ ocamapping.py
@@ -149,8 +149,6 @@ def run_recursive_scan(options, queue, results_q):
 
     for i in range(MAX_PROCESSES):
         folders_scan_pool.apply_async(dir_scan_worker, args=(i,))
-    folders_scan_pool.close()
-    folders_scan_pool.join()
 
     # for dirpath, dirnames, filenames in os.walk(options.path):
     #     for name in filenames:
