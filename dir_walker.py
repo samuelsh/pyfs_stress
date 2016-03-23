8109ae7816cdc7931fa4f91c541da4e9f4fa2f5d
samuelsh <samuel.sh79@gmail.com>
2016-03-23 13:17:13 +0200

    multiprocessing folder scan

--- dir_walker.py
+++ dir_walker.py
@@ -1,16 +1,39 @@
 import argparse
+import multiprocessing
 from multiprocessing.pool import Pool
 from multiprocessing import Manager
 import os
 
-MAX_POOL_SIZE = 1
 
-unsearched = Manager().Queue()
+class TreeCrawler(object):
+    def __init__(self, base_path, callback):
+        self.base_path = base_path
+        self.unsearched = Manager().Queue()
+        self.cpu_count = multiprocessing.cpu_count()
+        self.pool = Pool(self.cpu_count)
+        self.callback = callback
+        # acquire the list of all paths inside base path
+        self.first_level_dirs = next(os.walk(self.base_path))[1]
+
+        for path in self.first_level_dirs:
+            self.unsearched.put(args.path + "/" + path)
+
+    def __parallel_worker(self, task_num):
+        while True:
+            dirpath = self.unsearched.get()
+            dirs = self.callback(task_num, dirpath)
+            for newdir in dirs:
+                self.unsearched.put(newdir)
+            self.unsearched.task_done()
+
+    def run_crawler(self):
+        self.pool.map_async(self.__parallel_worker, range(self.cpu_count))
+        self.pool.close()
+        self.unsearched.join()
 
 
 def explore_path(task_num, dirpath):
     directories = []
-    nondirectories = []
     print "Task: " + str(task_num) + " >>> Explored path: " + dirpath
     for filename in os.listdir(dirpath):
         fullname = os.path.join(dirpath, filename)
@@ -19,28 +42,10 @@ def explore_path(task_num, dirpath):
     return directories
 
 
-def parallel_worker(task_num):
-    while True:
-        dirpath = unsearched.get()
-        dirs = explore_path(task_num, dirpath)
-        for newdir in dirs:
-            unsearched.put(newdir)
-        unsearched.task_done()
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--path", help="file/dir path", action="store", dest="path", default=".", required=True)
 args = parser.parse_args()
 
-# acquire the list of paths
-first_level_dirs = next(os.walk(args.path))[1]
-
-for path in first_level_dirs:
-    unsearched.put(args.path + "/" + path)
-
-pool = Pool(MAX_POOL_SIZE)
-pool.map_async(parallel_worker, range(MAX_POOL_SIZE))
-pool.close()
-unsearched.join()
+crawler = TreeCrawler(args.path, explore_path)
 
 print 'Done'
