365b05cc620a66a5dda6b097be0bad1df843f645
samuelsh <samuel.sh79@gmail.com>
2016-03-24 13:24:14 +0200

    multiprocessing folder scan

--- dir_walker.py
+++ dir_walker.py
@@ -11,12 +11,20 @@ class TreeCrawler(object):
         self.unsearched = Manager().Queue()
         self.cpu_count = multiprocessing.cpu_count()
         self.pool = Pool(self.cpu_count)
+        self.__dirpath = ""
         self.callback = callback
         # acquire the list of all paths inside base path
         self.first_level_dirs = next(os.walk(self.base_path))[1]
 
         for path in self.first_level_dirs:
             self.unsearched.put(self.base_path + "/" + path)
+
+    def __explore_path(self):
+        directories = []
+        for filename in os.walk(self.__dirpath).next()[1]:
+            fullname = os.path.join(self.__dirpath, filename)
+            directories.append(fullname)
+        return directories
 
     def run_crawler(self):
         self.pool.map_async(self.parallel_worker, range(self.cpu_count))
@@ -25,27 +33,19 @@ class TreeCrawler(object):
 
     def parallel_worker(self, task_num):
         while True:
-            dirpath = self.unsearched.get()
-            dirs = self.callback(task_num, dirpath)
+            self.__dirpath = self.unsearched.get()
+            print "Task: " + str(task_num) + " >>> Explored path: " + self.__dirpath
+            dirs = self.__explore_path()
             for newdir in dirs:
                 self.unsearched.put(newdir)
             self.unsearched.task_done()
-
-
-def explore_path(task_num, dirpath):
-    directories = []
-    print "Task: " + str(task_num) + " >>> Explored path: " + dirpath
-    for filename in os.walk(dirpath).next()[1]:
-        fullname = os.path.join(dirpath, filename)
-        directories.append(fullname)
-    return directories
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--path", help="file/dir path", action="store", dest="path", default=".", required=True)
 args = parser.parse_args()
 
-crawler = TreeCrawler(args.path, explore_path)
+crawler = TreeCrawler(args.path)
 crawler.run_crawler()
 
 print 'Done'
