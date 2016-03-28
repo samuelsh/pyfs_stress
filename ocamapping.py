3eb1a2223c72d120bab5ba54a10398bedadd86b5
samuelsh <samuel.sh79@gmail.com>
2016-03-28 15:04:32 +0300

    multiprocessing folder scan

--- ocamapping.py
+++ ocamapping.py
@@ -176,7 +176,7 @@ def run_recursive_scan(options, results_q):
     # for p in process_pool:
     #     p.join()
     process_pool.close()
-    process_pool.join()
+    #process_pool.join()
 
     while not results_q.empty():
         q = results_q.get()
