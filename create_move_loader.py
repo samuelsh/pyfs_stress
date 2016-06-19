9f2c7df34fda915035af9d4186f07d065303eab6
samuelsh <samuel.sh79@gmail.com>
2016-06-19 16:21:12 +0300

    Minor fixes

--- create_move_loader.py
+++ create_move_loader.py
@@ -82,6 +82,7 @@ def renamer_worker(args, proc_name, lock):
     global stop_event
     while not stop_event.is_set():
         try:
+            lock.acquire(blocking=0)
             # Getting all file in folder
             files_list = os.listdir("%s/%s" % (args.mount_point, args.test_dir))
             print("Process %s -- Got dirlist at %s/%s" % (proc_name, args.mount_point, args.test_dir))
@@ -90,18 +91,15 @@ def renamer_worker(args, proc_name, lock):
                     new_file_name = test_file.replace('created', 'moved')
                     print(
                         "renaming %s to %s at path %s/%s" % (test_file, new_file_name, args.mount_point, args.test_dir))
-                    lock.acquire(blocking=0)
                     os.rename("%s/%s/%s" % (args.mount_point, args.test_dir, test_file),
                               "%s/%s/%s" % (args.mount_point, args.test_dir, new_file_name))
-                    lock.release()
                 elif "moved" in test_file:
                     new_file_name = test_file.replace('moved', 'created')
                     print(
                         "renaming %s to %s at path %s/%s" % (test_file, new_file_name, args.mount_point, args.test_dir))
-                    lock.acquire(blocking=0)
                     os.rename("%s/%s/%s" % (args.mount_point, args.test_dir, test_file),
                               "%s/%s/%s" % (args.mount_point, args.test_dir, new_file_name))
-                    lock.release()
+            lock.release()
 
         except OSError as rename_worker_exception:
             traceback.print_exc(rename_worker_exception)
