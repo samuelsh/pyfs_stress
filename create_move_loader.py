b8c0a5412383ecb1a1999769babef04c120f82e7
samuelsh <samuel.sh79@gmail.com>
2016-06-20 06:46:07 +0300

    locking issues

--- create_move_loader.py
+++ create_move_loader.py
@@ -59,16 +59,18 @@ def init_test(args, logger):
 def file_creator_worker(path, proc_id, lock):
     global total_files
     while total_files.value < MAX_FILES:
+        print("### DEBUG: %s -- going to lock total_files", proc_id)
         lock.acquire()
-        print("### DEBUG: going to lock total_files")
+        print("### DEBUG: %s -- lock aquired on total_files", proc_id)
         filenum = total_files.value
         total_files.value += 1
-        print("### DEBUG: going to release total_files")
+        print("### DEBUG: %s -- going to release total_files", proc_id)
         lock.release()
+        print("### DEBUG: %s -- total_files released", proc_id)
         print("Creating %s/file_created_client_#%d_file_number_#%d" % (
             path, proc_id, filenum))
         touch('%s/file_created_client_#%d_file_number_#%d' % (path, proc_id, filenum))
-    print("Done Creating files! total: %d" % int(total_files.value))
+    print("%s -- Done Creating files! total: %d" % (int(total_files.value), proc_id))
 
 
 def file_creator(args, path, logger):
