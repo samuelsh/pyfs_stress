077dd6d4aaad4620a7b60f3ac82b17d8fe66fb9d
samuels <samuel.sh79@gmail.com>
2016-09-14 13:13:07 +0300

    - Implementing dynamo actions [mkdir, list, touch, delete]

--- client/dynamo.py
+++ client/dynamo.py
@@ -70,7 +70,7 @@ class Dynamo(object):
             elif action == 'list':
                 os.listdir('{0}/{1}'.format(CLIENT_MOUNT_POINT, work['target']))
         except Exception as work_error:
-            return "{0}".format(work_error)
+            return "{0}: {1}".format(action, work_error)
         result = "{0} on {1}".format(work['action'], work['target'])
         time.sleep(random.randint(1, 10))
         return result
