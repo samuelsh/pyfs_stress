99e0143fc7f30f7d78a80b6712b9393e9598b30c
samuels <samuel.sh79@gmail.com>
2016-09-15 15:31:50 +0300

    - Implementing dynamo actions [mkdir, list, touch, delete]

--- server/controller.py
+++ server/controller.py
@@ -81,7 +81,7 @@ class Controller(object):
                 target = "/{0}/{1}".format(rdir.tag, fname)
             elif action == 'stat':
                 rdir = self._dir_tree.get_random_dir()
-                fname = rdir.data.get_random_flie().name
+                fname = rdir.data.get_random_file().name
                 target = "/{0}/{1}".format(rdir.tag, fname)
             elif action == "list":
                 target = self._dir_tree.get_random_dir_name()
