8dfab44e41ef0cbd58f86b25971255f6bbf3058c
samuels <samuel.sh79@gmail.com>
2016-09-21 13:39:40 +0300

    - Added multidomain mount on clients - Added gssh

--- client/dynamo_starter.py
+++ client/dynamo_starter.py
@@ -62,18 +62,18 @@ def run():
         logger.debug("Active Nodes: %s" % active_nodes)
         domains = shell_utils.FSUtils.get_domains_num(args.server)
         logger.debug("FSD domains: %s" % domains)
+        logger.info("Mounting work path...")
+        if args.scenario == 'domains':
+            shell_utils.FSUtils.mount_fsd(args.cluster, args.export_dir, active_nodes, domains, 'nfs3', 'DIRSPLIT', '6')
+            #/mnt/DIRSPLIT-node0.g8-5
+            for i in range(active_nodes):
+                for j in randint(domains):
+                    if not os.path.ismount('/mnt/%s-node%d.%s-%d' % ('MOVER', i, args.cluster, j)):
+                        logger.error('mount_fsd failed!')
+                        raise RuntimeError
     except Exception as error_on_init:
         logger.error("".join(error_on_init) + " WorkDir: {0}".format(os.getcwd()))
         raise
-    logger.info("Mounting work path...")
-    if args.scenario == 'domains':
-        shell_utils.FSUtils.mount_fsd(args.cluster, args.export_dir, active_nodes, domains, 'nfs3', 'DIRSPLIT', '6')
-        #/mnt/DIRSPLIT-node0.g8-5
-        for i in range(active_nodes):
-            for j in randint(domains):
-                if not os.path.ismount('/mnt/%s-node%d.%s-%d' % ('MOVER', i, args.cluster, j)):
-                    logger.error('mount_fsd failed!')
-                    raise RuntimeError
     # Start a few worker processes
     for i in range(MAX_WORKERS_PER_CLIENT):
         processes.append(Process(target=run_worker, args=(logger, stop_event, args.controller, args.server, i,)))
