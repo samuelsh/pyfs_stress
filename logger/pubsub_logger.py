dd0b96b9360b9af7f694caadad3ec0952a6d556b
samuels <samuel.sh79@gmail.com>
2016-10-13 11:15:52 +0300

    - PUBSUB logger -- fixing binding issues

--- logger/pubsub_logger.py
+++ logger/pubsub_logger.py
@@ -39,7 +39,7 @@ class SUBLogger:
 
         self.ctx = zmq.Context()
         self._sub = self.ctx.socket(zmq.SUB)
-        self._sub.bind('tcp://localhost:{0}'.format(port))
+        self._sub.bind('tcp://127.0.0.1:{0}'.format(port))
         self._sub.setsockopt(zmq.SUBSCRIBE, "")
         # create console handler and set level to info
         handler = logging.StreamHandler(sys.stdout)
