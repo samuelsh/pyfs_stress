e308e874bd4490f53254fca1e099bff3fda112c5
samuels <samuel.sh79@gmail.com>
2016-11-08 12:44:02 +0200

    Improving DynamoException

--- config/error_codes.py
+++ config/error_codes.py
@@ -1,9 +1,9 @@
 """
-This file provides internal test error codes for any error that raises DynamoException
+This file provides internal error codes for any error that raises DynamoException
 """
 
 __author__ = 'samuels'
 
-SAMEFILE = 0x10000  # attempt to move file to itself from different mount points (devices)
-NO_TARGET = 0x10001  # Target not specified
-MAX_DIR_SIZE = 0x10002  # Directory entry reached max size limit
+SAMEFILE = 0x100  # attempt to move file to itself from different mount points (devices)
+NO_TARGET = 0x101  # Target not specified
+MAX_DIR_SIZE = 0x102  # Directory entry reached max size limit
