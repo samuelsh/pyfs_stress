ffbaffc6a9ee9ba8026433bbed4089cd44ad02ac
samuels <samuel.sh79@gmail.com>
2016-11-10 11:49:36 +0200

    Message format from controller to clients changed: now 'data' filed will hold all relevant info, including target path

--- client/dynamo.py
+++ client/dynamo.py
@@ -113,10 +113,11 @@ class Dynamo(object):
         mount_point = "".join(
             "/mnt/DIRSPLIT-node{0}.{1}-{2}".format(random.randint(0, self.nodes - 1), self._server,
                                                    random.randint(0, self.domains - 1)))
-        self.logger.debug('Incoming job: \'{0}\' on \'{1}\''.format(work['action'], work['target']))
+        self.logger.debug('Incoming job: \'{0}\' on \'{1}\''.format(work['action'], work['data']['target']))
         try:
             if 'None' in work['data']['target']:
-                raise DynamoException(error_codes.NO_TARGET, "{0}".format("Target not specified", work['target']))
+                raise DynamoException(error_codes.NO_TARGET,
+                                      "{0}".format("Target not specified", work['data']['target']))
             response = response_action(action, mount_point, work['data'], nodes=self.nodes, server=self._server,
                                        domains=self.domains)
             if response:
