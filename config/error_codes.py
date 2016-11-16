a0d183b4d9df5851361898acf7f94df8e5046561
samuels <samuel.sh79@gmail.com>
2016-11-16 08:47:51 +0200

    read method will rise exception if enforced to read empty file. New util added: file names generator with hash collision

--- config/error_codes.py
+++ config/error_codes.py
@@ -7,3 +7,4 @@ __author__ = 'samuels'
 SAMEFILE = 0x100  # attempt to move file to itself from different mount points (devices)
 NO_TARGET = 0x101  # Target not specified
 MAX_DIR_SIZE = 0x102  # Directory entry reached max size limit
+ZERO_SIZE = 0x103  # Empty file. Can happen due to raise, when process trying to read file, which is still empty
---
+++ string_generator.py
@@ -0,0 +1,73 @@
+"""
+Alphanumeric string generator
+"""
+import argparse
+import sys
+
+import utils.shell_utils
+
+__author__ = 'samuels'
+
+PATH_TO_HASH_TOOL = "/zebra/qa/samuels/misc/hash_tool"
+
+
+def store_console():
+    pass
+
+
+def store_file():
+    pass
+
+
+def store_sqlite():
+    pass
+
+
+def store_redis():
+    pass
+
+
+def generate_random_string_hc(hc_value):
+    while 1:
+        generated_string = utils.shell_utils.StringUtils.get_random_string_nospec(64)
+        generated_hash = utils.shell_utils.ShellUtils.run_shell_command("/zebra/qa/samuels/misc/hash_tool",
+                                                                        '{0} 6'.format(generated_string))
+        generated_hash = int(generated_hash)
+        if hc_value == generated_hash:
+            return generated_string
+
+
+def get_args():
+    """
+    Supports the command-line arguments listed below.
+    """
+
+    parser = argparse.ArgumentParser(
+        description='String generator')
+    parser.add_argument('--length', type=str, default=64, help="String Length")
+    parser.add_argument('--hc', action='store_true', help="Force hash collision")
+    parser.add_argument('--count', type=int, default=10, help="Number of strings to generate")
+    parser.add_argument('--store', type=str, required=True, choices=['console', 'file', 'sqlite', 'redis'],
+                        help="Where to store generated data")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+    store_method = {
+        'console': store_console,
+        'file': store_file,
+        'sqlite': store_sqlite,
+        'redis': store_redis
+    }
+
+    if args.hc:
+        for _ in range(args.count):
+            print generate_random_string_hc(45)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception:
+        sys.exit(1)
