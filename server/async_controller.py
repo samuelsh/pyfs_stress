d11ef1ef6d46545cfcfc6df34f0018da06a59de2
samuels <samuel.sh79@gmail.com>
2016-11-24 16:00:58 +0200

    Working on correct load balancing

--- server/async_controller.py
+++ server/async_controller.py
@@ -90,8 +90,8 @@ class Controller(object):
 
     @property
     def get_next_job(self):
-        actions = [('mkdir', 10), ('list', 5), ('delete', 10), ('touch', 30), ('stat', 5), ('read', 10), ('rename', 5),
-                   ('rename_exist', 5), ('write', 15), ('truncate', 5)]
+        actions = [('mkdir', 5), ('list', 5), ('delete', 10), ('touch', 50), ('stat', 5), ('read', 10), ('rename', 5),
+                   ('rename_exist', 5), ('write', 5), ('truncate', 5)]
         while True:
             action = weighted_choice(actions)
             # if some client disconnected, messages assigned to him won't be lost
