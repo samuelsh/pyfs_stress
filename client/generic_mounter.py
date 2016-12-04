0b38f799f60b28f26ec4be99f3b07004dcdaae1c
samuels <samuel.sh79@gmail.com>
2016-12-04 10:05:47 +0200

    Fefature: Filename will be created with random length 1 - 64

--- client/generic_mounter.py
+++ client/generic_mounter.py
@@ -1,6 +1,7 @@
 import os
 
 import errno
+import random
 import socket
 
 from logger import server_logger
@@ -11,22 +12,43 @@ __author__ = "samuels"
 MOUNT_BASE = "/mnt"
 
 
-def mount(server, export, mount_type, prefix, logger=None):
-    if not logger:
-        logger = server_logger.ConsoleLogger(socket.gethostname()).logger
-    mount_point = prefix + '_' + export
-    try:
-        os.makedirs('{0}/{1}'.format(MOUNT_BASE, mount_point))
-    except OSError as os_error:
-        if os_error.errno == errno.EEXIST:
-            pass
+class Mounter:
+    def __init__(self, server, export, mount_type, prefix, logger=None):
+        self.logger = logger
+        self.prefix = prefix
+        self.mount_type = mount_type
+        self.server = server
+        self.export = export
+        self.mount_points = []
+
+    def mount(self):
+        if not self.logger:
+            logger = server_logger.ConsoleLogger(socket.gethostname()).logger
         else:
-            logger.error(os_error)
-            raise os_error
-    try:
-        shell_utils.ShellUtils.run_shell_command('umount', '-fl {0}/{1}'.format(MOUNT_BASE, mount_point))
-    except Exception as e:
-        logger.warn(e)
-    shell_utils.ShellUtils.run_shell_command('mount',
-                                             '-o nfsvers={0} {1}/{2}:/{3}'.format(mount_type, server, export,
-                                                                                  mount_point))
+            logger = self.logger
+        mount_point = MOUNT_BASE + '/' + self.prefix + '_' + self.export + '_' + self.server
+        try:
+            os.makedirs('{0}/{1}'.format(MOUNT_BASE, mount_point))
+        except OSError as os_error:
+            if os_error.errno == errno.EEXIST:
+                pass
+            else:
+                logger.error(os_error)
+                raise os_error
+        try:
+            shell_utils.ShellUtils.run_shell_command('umount', '-fl {0}'.format(mount_point))
+        except RuntimeError as e:
+            logger.warn(e)
+        shell_utils.ShellUtils.run_shell_command('mount',
+                                                 '-o nfsvers={0} {1}:/{2} {3}'.format(self.mount_type, self.server,
+                                                                                      self.export, mount_point))
+        self.mount_points.append(mount_point)
+        if not os.path.ismount(mount_point):
+            logger.error('mount failed! type: {0} server: {1} export: {2} mount point: {3}'.format(self.mount_type,
+                                                                                                   self.server,
+                                                                                                   self.export,
+                                                                                                   mount_point))
+            raise RuntimeError
+
+    def get_random_mountpoint(self):
+        return random.choice(self.mount_points)
