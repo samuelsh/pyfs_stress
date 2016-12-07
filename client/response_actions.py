ff712657bd5e3cbade0033da5da44bda63636f24
samuels <samuel.sh79@gmail.com>
2016-12-07 10:25:31 +0200

    Fix: removing useless lock on truncate

--- client/response_actions.py
+++ client/response_actions.py
@@ -138,7 +138,7 @@ def write(mount_point, incoming_data, **kwargs):
     hasher.update(pattern_to_write)
     data_hash = hasher.hexdigest()
     with open("{0}{1}".format(mount_point, incoming_data['target']), 'r+') as f:
-        fcntl.lockf(f.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB, data_pattern['repeats'], offset, 0)
+        # fcntl.lockf(f.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB, data_pattern['repeats'], offset, 0)
         f.seek(offset)
         f.write(pattern_to_write)
         f.flush()
@@ -149,10 +149,10 @@ def write(mount_point, incoming_data, **kwargs):
         hasher = hashlib.md5()
         hasher.update(buf)
         if hasher.hexdigest() != data_hash:
-            fcntl.lockf(f.fileno(), fcntl.LOCK_UN)
+            # fcntl.lockf(f.fileno(), fcntl.LOCK_UN)
             raise DynamoException(error_codes.HASHERR, "Data patter verification on disk failed!",
                                   incoming_data['target'])
-        fcntl.lockf(f.fileno(), fcntl.LOCK_UN)
+        # fcntl.lockf(f.fileno(), fcntl.LOCK_UN)
     outgoing_data['data_pattern'] = data_pattern['pattern']
     outgoing_data['repeats'] = data_pattern['repeats']
     outgoing_data['hash'] = data_hash
