6527ad8fc235428cc009e9d70df3795c3024630c
samuels <samuel.sh79@gmail.com>
2017-01-01 15:12:25 +0200

    Fix: Test statistics print formatting

--- server/collector.py
+++ server/collector.py
@@ -25,9 +25,9 @@ class Collector:
                                               .format(self.test_stats['failed']['total'])))
             self.logger.info("{0:^80}".format("=== Successful operations stats ==="))
             for k, v in self.test_stats['success'].items():
-                self.logger.info("{0:>40}".format("{0}: {1}".format(k, v)))
+                self.logger.info("{0}".format("{0}: {1}".format(k, v)).rjust(40))
             self.logger.info("{0:^80}".format("=== Failed operations stats ==="))
             for k, v in self.test_stats['failed'].items():
-                self.logger.info("{0:>40}".format("{0}: {1}".format(k, v)))
+                self.logger.info("{0}".format("{0}: {1}".format(k, v)).rjust(40))
             time.sleep(10)
 
