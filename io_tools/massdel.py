9f607dabf4a4037ec23f1d8c0f040c60bc984784
samuel <samuel@vastdata.com>
2019-11-06 13:50:52 +0200

    Starting devel branch, for major changes and refactoring

--- io_tools/massdel.py
+++ io_tools/massdel.py
@@ -16,18 +16,23 @@ from threading import Event
 from client.generic_mounter import Mounter
 from logger.server_logger import ConsoleLogger
 
-logger = None
-stop_event = None
-
 MAX_SCANNING_THREADS = 100
 threads_count = 0
 total_scanned_files = 0
+total_scanned_dirs = 0
+read_files = 0
 deleted_files = 0
 renamed_files = 0
 
 scan_lock = threading.Lock()
+dirs_cnt_lock = threading.Lock()
+files_cnt_lock = threading.Lock()
 delete_lock = threading.Lock()
 rename_lock = threading.Lock()
+
+logger = None
+stop_event = None
+scan_threads_semaphore = threading.BoundedSemaphore(MAX_SCANNING_THREADS)
 
 
 def get_args():
@@ -58,53 +63,65 @@ class TestStatsCollector(threading.Timer):
 
 
 def print_stats(dirs_queue):
-    logger.info(f"### Total spawned workers: {threads_count} Queue depth: {dirs_queue.qsize()} ###")
-    logger.info(f"### Deleted Files: {deleted_files} Renamed Files: {renamed_files}")
+    logger.info(f"### Total spawned scanner workers: {threads_count} Read Queue depth: {dirs_queue.qsize()} ###")
+    logger.info(f"### Total Read Files: {read_files} ###")
+    logger.info(f"### Total Scanned Files: {total_scanned_files} ###")
+    logger.info(f"### Total Scanned Dirs: {total_scanned_dirs} ###")
+    logger.info(f"### Deleted Files: {deleted_files} Renamed Files: {renamed_files} ###")
 
 
-def dir_scanner(files_queue, root_dir):
-    global stop_event, threads_count, total_scanned_files
+def dir_scanner(files_queue, mount_points, root_dir, next_mp):
+    global stop_event, threads_count, total_scanned_files, total_scanned_dirs
+    ident = threading.get_ident()
     try:
-        logger.debug(f"Scanner Worker {threading.get_ident()}: Scanning {root_dir}")
-        with os.scandir(root_dir) as dirs_iterator:
+        with scan_lock:
+            threads_count += 1
+        full_root_path = "/".join([mount_points[next_mp], root_dir])
+        logger.info(f"Scanner Worker {ident}: Scanning: {full_root_path}")
+        with os.scandir(full_root_path) as dirs_iterator:
             for entry in dirs_iterator:
                 if stop_event.is_set():
                     return
                 if entry.is_dir():
-                    logger.info(f"Entry {entry.name} is Directory. Spawning new thread...")
-                    scan_thread = threading.Thread(target=dir_scanner, args=(files_queue, entry.path))
-                    scan_thread.start()
-                    with scan_lock:
-                        threads_count += 1
+                    with dirs_cnt_lock:
+                        total_scanned_dirs += 1
                     if threads_count >= MAX_SCANNING_THREADS:
-                        logger.info(f"Number of threads reached {MAX_SCANNING_THREADS}. "
-                                    f"Waiting any thread to finish...")
-                        scan_thread.join()
-                        with scan_lock:
-                            threads_count -= 1
+                        logger.debug(f"Number of threads reached {MAX_SCANNING_THREADS}. "
+                                     f"Waiting any thread to finish...")
+                    with scan_threads_semaphore:
+                        logger.debug(f"Entry {entry.name} is Directory. Spawning new thread...")
+                        next_mp += 1
+                        if next_mp >= len(mount_points):
+                            next_mp = 0
+                        new_entry_path = list(filter(None, entry.path.split("/")))[2:]
+                        new_entry_path = "/".join(new_entry_path)
+                        scan_thread = threading.Thread(target=dir_scanner, args=(files_queue, mount_points,
+                                                                                 new_entry_path, next_mp))
+                        scan_thread.start()
                 else:
-                    logger.debug(f"Entry {entry.name} is File. Putting {entry.path}")
                     files_queue.put(entry.path)
-                    with scan_lock:
+                    with files_cnt_lock:
                         total_scanned_files += 1
-                    # logger.info(f"thread {threading.get_ident()} decreasing thread_count: {threads_count}")
-            logger.debug(f"Scanner Worker {threading.get_ident()}: Done Scanning {root_dir}")
+            logger.debug(f"Scanner Worker {ident}: Done Scanning {root_dir}")
+            with scan_lock:
+                threads_count -= 1
         if threads_count <= 0:
-            logger.info(f"Scanner Worker {threading.get_ident()}: Spawned threads {threads_count}. "
+            logger.info(f"Scanner Worker {ident}: Spawned threads {threads_count}. "
                         f"Done scanning, waiting for all worker threads to complete")
     except OSError as e:
         if e.errno == errno.EACCES:
-            logger.warn(f"Scanner Woker {threading.get_ident()} failed due to {e} and will be stopped")
+            logger.warn(f"Scanner Worker {ident} failed due to {e} and will be stopped")
         else:
-            logger.exception(f"Scanner Worker {threading.get_ident()} Error: {e}. Shutting down...")
+            logger.exception(f"Scanner Worker {ident} Error: {e}. Shutting down...")
             stop_event.set()
 
 
 def rename_worker(dirs_queue):
     global stop_event, renamed_files
+    ident = threading.get_ident()
     src_path = ""
     dst_path = ""
-    logger.info(f"Renamer Worker {threading.get_ident()} started...")
+    logger.info(f"Renamer Worker {ident} started...")
     while not stop_event.is_set():
         try:
             src_path = dirs_queue.get(timeout=10)
@@ -112,27 +129,32 @@ def rename_worker(dirs_queue):
             os.rename(src_path, dst_path)
             with rename_lock:
                 renamed_files += 1
+        except FileNotFoundError:
+            pass
         except OSError as e:
-            logger.error(f"Renamer Worker {threading.get_ident()} Error: {e}, Src: {src_path}, Dst: {dst_path}")
+            logger.error(f"Renamer Worker {ident} Error: {e}, Src: {src_path}, Dst: {dst_path}")
         except queue.Empty:
-            logger.error(f"Empty queue: Renamer Worker {threading.get_ident()} done and exits.")
+            logger.error(f"Empty queue: Renamer Worker {ident} done and exits.")
             return
 
 
 def delete_worker(dirs_queue):
     global stop_event, deleted_files
+    ident = threading.get_ident()
     full_path = ""
-    logger.info(f"Deleter Worker {threading.get_ident()} started...")
+    logger.info(f"Deleter Worker {ident} started...")
     while not stop_event.is_set():
         try:
             full_path = dirs_queue.get(timeout=60)
             os.remove(full_path)
             with delete_lock:
                 deleted_files += 1
+        except FileNotFoundError:
+            pass
         except OSError as e:
-            logger.error(f"Deleter Worker {threading.get_ident()} Error: {e}, Path: {full_path}")
+            logger.error(f"Deleter Worker {ident} Error: {e}, Path: {full_path}")
         except queue.Empty:
-            logger.error(f"Empty queue: Deleter Worker {threading.get_ident()} done and exits.")
+            logger.error(f"Empty queue: Deleter Worker {ident} done and exits.")
             return
 
 
@@ -140,7 +162,7 @@ def main():
     global logger, stop_event, threads_count
     logger = ConsoleLogger('md_massdel').logger
     stop_event = Event()
-    dirs_queue = queue.Queue()
+    files_queue = queue.Queue()
 
     args = get_args()
     logger.info("Mounting work path...")
@@ -153,24 +175,18 @@ def main():
         mounter.mount()
 
     futures = []
-    mp = mounter.get_random_mountpoint()
-    test_dir = os.path.join(mp, args.test_dir)
-    logger.info("Selected mountpoint to be scanned: {}".format(test_dir))
-    stats_collector = TestStatsCollector(print_stats, args=[dirs_queue, ])
+    mount_points = mounter.mount_points
+    stats_collector = TestStatsCollector(print_stats, args=[files_queue, ])
     stats_collector.start()
     logger.info("Workers ThreadPool started")
     with ThreadPoolExecutor() as executor:
-        futures.append(executor.submit(dir_scanner, dirs_queue, test_dir))
+        futures.append(executor.submit(dir_scanner, files_queue, mount_points, args.test_dir, 0))
         if args.action == "all" or args.action == "rename":
-            for _ in range(100):
-                futures.append(executor.submit(rename_worker, dirs_queue))
+            for _ in range(64):
+                futures.append(executor.submit(rename_worker, files_queue))
         if args.action == "all" or args.action == "delete":
-            for _ in range(100):
-                futures.append(executor.submit(delete_worker, dirs_queue))
-    logger.info("Waiting for all threads to complete...")
-    while threads_count >= 0:
-        time.sleep(0.1)
-    logger.info("All scanning threads completed...")
+            for _ in range(64):
+                futures.append(executor.submit(delete_worker, files_queue))
     for future in futures:
         try:
             logger.info(f'{"Job Done OK" if not future.result() else ""}')
--- io_tools/massread.py
+++ io_tools/massread.py
@@ -16,27 +16,30 @@ from threading import Event
 from client.generic_mounter import Mounter
 from logger.server_logger import ConsoleLogger
 
-logger = None
-stop_event = None
-scan_threads_semaphore = threading.Semaphore(64)
-
 MAX_SCANNING_THREADS = 64
 KB1 = 1024
 MB1 = KB1 * 1024
+COLLECTOR_INTERVAL = 60
 
 threads_count = 0
 total_scanned_files = 0
 total_scanned_dirs = 0
+total_scanned_snapshots = 0
 read_files = 0
 
 scan_lock = threading.Lock()
 dirs_cnt_lock = threading.Lock()
 files_cnt_lock = threading.Lock()
 read_lock = threading.Lock()
+snap_cnt_lock = threading.Lock()
+
+logger = None
+stop_event = None
+scan_threads_semaphore = threading.BoundedSemaphore(MAX_SCANNING_THREADS)
 
 
 class TestStatsCollector(threading.Timer):
-    def __init__(self, func, args=None, interval=60):
+    def __init__(self, func, args=None, interval=COLLECTOR_INTERVAL):
         """
         :param func: func
         :param args: set
@@ -61,45 +64,70 @@ def open_file(path):
         pass
 
 
+def stat_file(path):
+    os.stat(path)
+
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--cluster", help="Cluster Name", required=True, type=str)
     parser.add_argument("-e", "--export", help="NFS Export", default="/", type=str)
     parser.add_argument("-d", "--test_dir", help="Directory under test", default="", type=str)
     parser.add_argument("-s", "--skipread", help="Skip read. Open only", action="store_true")
+    parser.add_argument("-t", "--threads", help="Max scanning threads", type=int, default=MAX_SCANNING_THREADS)
+    parser.add_argument("--snapshots", help="Allow read snapshots directory", action="store_true")
     parser.add_argument('--start_vip', type=str, help="Start VIP address range")
     parser.add_argument('--end_vip', type=str, help="End VIP address range")
     return parser.parse_args()
 
 
 def print_stats(dirs_queue):
-    logger.info(f"### Total spawned workers: {threads_count} Queue depth: {dirs_queue.qsize()} ###")
-    logger.info(f"### Total Read Files: {read_files}")
-    logger.info(f"### Total Scanned Files: {total_scanned_files}")
-    logger.info(f"### Total Scanned Dirs: {total_scanned_dirs}")
+    logger.info(f"### Total spawned scanner workers: {threads_count} Read Queue depth: {dirs_queue.qsize()} ###")
+    logger.info(f"### Total Read Files: {read_files} ###")
+    logger.info(f"### Total Scanned Files: {total_scanned_files} ###")
+    logger.info(f"### Total Scanned Dirs: {total_scanned_dirs} ###")
+    logger.info(f"### Total Scanned Snapshots: {total_scanned_snapshots} ###")
 
 
-def dir_scanner(files_queue, root_dir):
+def dir_scanner(files_queue, mount_points, root_dir, next_mp, max_scanning_threads=MAX_SCANNING_THREADS,
+                read_snapshots=False):
     global stop_event, threads_count, total_scanned_files, total_scanned_dirs
     ident = threading.get_ident()
     try:
-        logger.info(f"Scanner Worker {ident}: Scanning {root_dir}")
         with scan_lock:
             threads_count += 1
-        with os.scandir(root_dir) as dirs_iterator:
+        full_root_path = "/".join([mount_points[next_mp], root_dir])
+        logger.info(f"Scanner Worker {ident}: Scanning: {full_root_path}")
+        with os.scandir(full_root_path) as dirs_iterator:
             for entry in dirs_iterator:
                 if stop_event.is_set():
                     return
                 if entry.is_dir():
                     with dirs_cnt_lock:
                         total_scanned_dirs += 1
-                    if threads_count >= MAX_SCANNING_THREADS:
-                        logger.info(f"Number of threads reached {MAX_SCANNING_THREADS}. "
-                                    f"Waiting any thread to finish...")
+                    if threads_count >= max_scanning_threads:
+                        logger.debug(f"Number of threads reached {max_scanning_threads}. "
+                                     f"Waiting any thread to finish...")
                     with scan_threads_semaphore:
                         logger.debug(f"Entry {entry.name} is Directory. Spawning new thread...")
-                        scan_thread = threading.Thread(target=dir_scanner, args=(files_queue, entry.path))
+                        next_mp += 1
+                        if next_mp >= len(mount_points):
+                            next_mp = 0
+                        new_entry_path = list(filter(None, entry.path.split("/")))[2:]
+                        new_entry_path = "/".join(new_entry_path)
+                        scan_thread = threading.Thread(target=dir_scanner, args=(files_queue, mount_points,
+                                                                                 new_entry_path, next_mp,
+                                                                                 read_snapshots))
                         scan_thread.start()
+                    if read_snapshots:
+                        with scan_threads_semaphore:
+                            snap_path = list(filter(None, full_root_path.split("/")))[2:]
+                            snap_path = "/".join(snap_path + [".snapshot"])
+                            snap_thread = threading.Thread(target=snap_scanner, args=(files_queue, mount_points,
+                                                                                      snap_path, next_mp,
+                                                                                      read_snapshots))
+                            snap_thread.start()
+
                 else:
                     files_queue.put(entry.path)
                     with files_cnt_lock:
@@ -118,33 +146,85 @@ def dir_scanner(files_queue, root_dir):
             stop_event.set()
 
 
-def reader_worker(dirs_queue, skip_read):
+def snap_scanner(files_queue, mount_points, root_dir, next_mp, max_scanning_threads=MAX_SCANNING_THREADS,
+                 read_snapshots=False):
+    global stop_event, threads_count, total_scanned_files, total_scanned_dirs, total_scanned_snapshots
+    ident = threading.get_ident()
+    try:
+        with scan_lock:
+            threads_count += 1
+        full_root_path = "/".join([mount_points[next_mp], root_dir])
+        if not os.path.exists(full_root_path):
+            return
+        logger.info(f"Snap Scanner Worker {ident}: Scanning: {full_root_path}")
+        with os.scandir(full_root_path) as dirs_iterator:
+            for entry in dirs_iterator:
+                if stop_event.is_set():
+                    return
+                if entry.is_dir():
+                    with dirs_cnt_lock:
+                        total_scanned_dirs += 1
+                    if threads_count >= max_scanning_threads:
+                        logger.debug(f"Number of threads reached {max_scanning_threads}. "
+                                     f"Waiting any thread to finish...")
+                    with scan_threads_semaphore:
+                        logger.debug(f"Entry {entry.name} is Directory. Spawning new thread...")
+                        next_mp += 1
+                        if next_mp >= len(mount_points):
+                            next_mp = 0
+                        new_entry_path = list(filter(None, entry.path.split("/")))[2:]
+                        new_entry_path = "/".join(new_entry_path)
+                        scan_thread = threading.Thread(target=dir_scanner, args=(files_queue, mount_points,
+                                                                                 new_entry_path, next_mp, False))
+                        scan_thread.start()
+                else:
+                    files_queue.put(entry.path)
+                    with files_cnt_lock:
+                        total_scanned_files += 1
+            with snap_cnt_lock:
+                total_scanned_snapshots += 1
+            logger.debug(f"Scanner Worker {ident}: Done Scanning {root_dir}")
+            with scan_lock:
+                threads_count -= 1
+        if threads_count <= 0:
+            logger.info(f"Scanner Worker {ident}: Spawned threads {threads_count}. "
+                        f"Done scanning, waiting for all worker threads to complete")
+    except OSError as e:
+        if e.errno == errno.EACCES:
+            logger.warn(f"Scanner Worker {ident} failed due to {e} and will be stopped")
+        else:
+            logger.exception(f"Scanner Worker {ident} Error: {e}. Shutting down...")
+            stop_event.set()
+
+
+def reader_worker(files_queue, skip_read):
     global stop_event, read_files
     ident = threading.get_ident()
     full_path = ""
     logger.info(f"Reader Worker {ident} started...")
     while not stop_event.is_set():
         try:
-            full_path = dirs_queue.get(timeout=10)
+            full_path = files_queue.get(timeout=0.1)
             if skip_read:
-                open_file(full_path)
+                stat_file(full_path)
             else:
                 read_file(full_path)
-
             with read_lock:
                 read_files += 1
+        except FileNotFoundError:
+            pass
         except OSError as e:
             logger.error(f"Reader Worker {ident} Error: {e}, Path: {full_path}")
         except queue.Empty:
-            logger.error(f"Empty queue: Reader Worker {ident} done and exits.")
+            logger.info(f"Empty queue: Reader Worker {ident} done and exits.")
             return
 
 
 def main():
-    global logger, stop_event, wait_event
+    global logger, stop_event
     logger = ConsoleLogger('mass_reader').logger
     stop_event = Event()
-    dirs_queue = queue.Queue()
+    files_queue = queue.Queue()
 
     args = get_args()
     logger.info("Mounting work path...")
@@ -157,16 +237,16 @@ def main():
         mounter.mount()
 
     futures = []
-    mp = mounter.get_random_mountpoint()
-    test_dir = os.path.join(mp, args.test_dir)
-    logger.info(f"Selected mountpoint to be scanned: {test_dir}")
-    stats_collector = TestStatsCollector(print_stats, args=[dirs_queue, ])
+    mount_points = mounter.mount_points
+    scanning_threads = MAX_SCANNING_THREADS if not args.snapshots else MAX_SCANNING_THREADS // 2
+    stats_collector = TestStatsCollector(print_stats, args=[files_queue, ])
     stats_collector.start()
     logger.info("Workers ThreadPool started")
     with ThreadPoolExecutor() as executor:
-        futures.append(executor.submit(dir_scanner, dirs_queue, test_dir))
+        futures.append(executor.submit(dir_scanner, files_queue, mount_points, args.test_dir, 0,
+                                       read_snapshots=args.snapshots, max_scanning_threads=scanning_threads))
         for _ in range(64):
-            futures.append(executor.submit(reader_worker, dirs_queue, args.skipread))
+            futures.append(executor.submit(reader_worker, files_queue, args.skipread))
     for future in futures:
         try:
             logger.info("{}".format("Job Done OK" if not future.result() else ""))
